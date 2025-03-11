456f9fc115d4c0831788c203798b7aac37f339f5
Eduardo Menezes <drummeduardo@gmail.com>
2025-03-11 11:12:31 -0300

    Update svm.py

--- mit_6.86x/digit_recognition/mnist/part1/svm.py
+++ mit_6.86x/digit_recognition/mnist/part1/svm.py
@@ -6,16 +6,27 @@ from sklearn.svm import LinearSVC
 
 def one_vs_rest_svm(train_x, train_y, test_x):
     """
-    Trains a linear SVM for binary classifciation
+    Trains a linear SVM for binary classification.
 
     Args:
         train_x - (n, d) NumPy array (n datapoints each with d features)
         train_y - (n, ) NumPy array containing the labels (0 or 1) for each training data point
         test_x - (m, d) NumPy array (m datapoints each with d features)
+
     Returns:
-        pred_test_y - (m,) NumPy array containing the labels (0 or 1) for each test data point
+        pred_test_y - (m, ) NumPy array containing the labels (0 or 1) for each test data point
     """
-    raise NotImplementedError
+    # Initialize the LinearSVC model with given parameters
+    svm_model = LinearSVC(random_state=0, C=0.1)
+    
+    # Train the SVM model using the training data
+    svm_model.fit(train_x, train_y)
+    
+    # Predict labels for the test data
+    pred_test_y = svm_model.predict(test_x)
+    
+    return pred_test_y
+
 
 
 def multi_class_svm(train_x, train_y, test_x):
