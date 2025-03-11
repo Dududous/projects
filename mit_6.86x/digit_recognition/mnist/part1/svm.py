4dc81383a7bfc0d4e9a625d4dd610499c78f9dcf
Eduardo Menezes <drummeduardo@gmail.com>
2025-03-11 19:05:13 -0300

    Update svm.py

--- mit_6.86x/digit_recognition/mnist/part1/svm.py
+++ mit_6.86x/digit_recognition/mnist/part1/svm.py
@@ -27,20 +27,28 @@ def one_vs_rest_svm(train_x, train_y, test_x):
     
     return pred_test_y
 
-
-
 def multi_class_svm(train_x, train_y, test_x):
     """
-    Trains a linear SVM for multiclass classifciation using a one-vs-rest strategy
+    Trains a linear SVM for multiclass classification using a one-vs-rest strategy.
 
     Args:
         train_x - (n, d) NumPy array (n datapoints each with d features)
         train_y - (n, ) NumPy array containing the labels (int) for each training data point
         test_x - (m, d) NumPy array (m datapoints each with d features)
+
     Returns:
-        pred_test_y - (m,) NumPy array containing the labels (int) for each test data point
+        pred_test_y - (m, ) NumPy array containing the labels (int) for each test data point
     """
-    raise NotImplementedError
+    # Initialize the LinearSVC model
+    svm_model = LinearSVC(random_state=0, C=0.1)
+    
+    # Train the model on the training data
+    svm_model.fit(train_x, train_y)
+    
+    # Predict labels for the test data
+    pred_test_y = svm_model.predict(test_x)
+    
+    return pred_test_y
 
 
 def compute_test_error_svm(test_y, pred_test_y):
