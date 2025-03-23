68735a29505ab888c1941290303d2e6e4390f7d2
Eduardo Menezes <drummeduardo@gmail.com>
2025-03-23 19:19:24 -0300

    Update main.py

--- mit_6.86x/digit_recognition/mnist/part1/main.py
+++ mit_6.86x/digit_recognition/mnist/part1/main.py
@@ -130,12 +130,48 @@ print('softmax test_error=', run_softmax_on_MNIST(temp_parameter=1))
 
 def run_softmax_on_MNIST_mod3(temp_parameter=1):
     """
-    Trains Softmax regression on digit (mod 3) classifications.
+    Trains softmax regression on MNIST data with labels modified to be mod 3, 
+    classifies test data, computes test error, and plots the cost function.
 
-    See run_softmax_on_MNIST for more info.
+    Runs softmax_regression on the MNIST training set with labels mod 3 and computes 
+    the test error using the test set. It uses the following values for parameters:
+    alpha = 0.3
+    lambda = 1e-4
+    num_iterations = 150
+
+    Saves the final theta to ./theta_mod3.pkl.gz
+
+    Returns:
+        Final test error for mod 3 classification
     """
-    # YOUR CODE HERE
-    raise NotImplementedError
+    # Load MNIST data
+    train_x, train_y, test_x, test_y = get_MNIST_data()
+    
+    # Update labels to be mod 3
+    train_y_mod3, test_y_mod3 = update_y(train_y, test_y)
+    
+    # Train softmax regression with mod 3 labels
+    theta, cost_function_history = softmax_regression(
+        train_x, 
+        train_y_mod3, 
+        temp_parameter=temp_parameter, 
+        alpha=0.3, 
+        lambda_factor=1.0e-4, 
+        k=3,  # Only 3 classes: 0, 1, 2 (mod 3)
+        num_iterations=150
+    )
+    
+    # Plot cost function over time
+    plot_cost_function_over_time(cost_function_history)
+    
+    # Compute test error for mod 3 classification
+    test_error_mod3 = compute_test_error(test_x, test_y_mod3, theta, temp_parameter)
+    
+    # Save the model parameters theta obtained from calling softmax_regression to disk.
+    write_pickle_data(theta, "./theta_mod3.pkl.gz")
+    
+    return test_error_mod3
+
 
 
 # TODO: Run run_softmax_on_MNIST_mod3(), report the error rate
